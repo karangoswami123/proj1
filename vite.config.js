b1070f2031519e577f1c32ebc7ab86e894b1f2f9
karangoswami123 <145990688+karangoswami123@users.noreply.github.com>
2024-07-13 10:38:15 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,18 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
-// import { defineConfig } from 'vite';
-// import react from '@vitejs/plugin-react';
-
+// // https://vitejs.dev/config/
 // export default defineConfig({
 //   plugins: [react()],
-//   build: {
-//     rollupOptions: {
-//       external: ['axios'],
-//     },
-//   },
-// });
+// })
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+export default defineConfig({
+  plugins: [react()],
+  build: {
+    rollupOptions: {
+      external: ['axios'],
+    },
+  },
+});
